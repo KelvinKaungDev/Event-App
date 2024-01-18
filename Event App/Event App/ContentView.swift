8122e8423244d686c9ecb8b8915ebf45c7b3b7c3
Kelvin Kaung <u6511753@au.edu>
2024-01-18 15:19:11 +0700

    Event App Initial State

---
+++ Event App/Event App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Event App
+//
+//  Created by Kelvin Gao  on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Event App/Event App/Event_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_AppApp.swift
+//  Event App
+//
+//  Created by Kelvin Gao  on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Event_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
