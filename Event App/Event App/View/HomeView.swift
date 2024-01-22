dbafd498e47324ef6973d9d5bc4f68f4df290cf5
Skypoppi <austin.xu.me@gmail.com>
2024-01-22 13:17:56 +0700

    Modify host event ui

--- Event App/Event App/View/HomeView.swift
+++ Event App/Event App/View/HomeView.swift
@@ -12,49 +12,54 @@ struct HomeView : View {
                 ZStack{
                     Image("backgroundImage")
                         .edgesIgnoringSafeArea(.top)
-                    VStack(alignment: .leading, spacing:10){
-                        HStack(spacing:10){
-                            Image("profilePic")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .clipShape(Circle())
-                                .frame(width:45,height:45,alignment:.center)
-                                .shadow(color:.black,radius: 5,x:5,y:5)
-                            VStack(alignment:.leading){
-                                Text("Welcome Back")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size:14))
-                                    .fontWeight(.medium)
-                                Text("Patrick Swan")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size:20))
-                                    .fontWeight(.bold)
+                        .overlay(
+                            VStack(alignment: .leading, spacing:10){
+                                Spacer()
+                                    .frame(height: 15)
+                                HStack(spacing:10){
+                                    Image("profilePic")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(Circle())
+                                        .frame(width:45,height:45,alignment:.center)
+                                        .shadow(color:.black,radius: 5,x:5,y:5)
+                                    VStack(alignment:.leading){
+                                        Text("Welcome Back")
+                                            .foregroundStyle(.white)
+                                            .font(.system(size:14))
+                                            .fontWeight(.medium)
+                                        Text("Patrick Swan")
+                                            .foregroundStyle(.white)
+                                            .font(.system(size:20))
+                                            .fontWeight(.bold)
+                                        
+                                    }// end of VStack
+                                    .padding(.horizontal,4)
+                                    Spacer()
+                                    Image(systemName: "bell.fill")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size:25))
+                                    
+                                } .padding()
+                                SearchBarItemView(searchKeyword: $searchKeyword)
+                                    .padding(.vertical)
                                 
-                            }// end of VStack
-                            .padding(.horizontal,4)
-                            Spacer()
-                            Image(systemName: "bell.fill")
-                                .foregroundStyle(.white)
-                                .font(.system(size:25))
-                            
-                        } .padding()
-                        SearchBarItemView(searchKeyword: $searchKeyword)
+                                AllCategoryView()
+                                
+                                Text("Upcoming Events")
+                                    .font(.system(size:30))
+                                    .fontWeight(.bold)
+                                    .padding(.horizontal)
+                                    .foregroundStyle(.white)
+                                Spacer()
+                                
+                                AllEventsViews()
+                                
+                                
+                            } //end ofVStack
                             .padding(.vertical)
-                        
-                        AllCategoryView()
-                        
-                        Text("Upcoming Events")
-                            .font(.system(size:30))
-                            .fontWeight(.bold)
-                            .padding(.horizontal)
-                            .foregroundStyle(.white)
-                        Spacer()
-                        
-                        AllEventsViews()
-                        
-                        
-                    } //end ofVStack
-                    .padding(.vertical)
+                        )
+                    
                 } //end of ZStack
         }// end of NavigationStack
         .navigationBarBackButtonHidden(true)
