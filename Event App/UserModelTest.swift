f34cf2a0f1f13f5b251b934d28fe19f95bab7349
Kelvin Kaung <u6511753@au.edu>
2024-01-23 15:53:51 +0700

    Modified UserViewModel

--- Event App/UserModelTest.swift
+++ Event App/UserModelTest.swift
@@ -10,4 +10,10 @@ final class UserModelTest: XCTestCase {
         let adminData = try! JSONDecoder().decode(UserModel.self, from: jsonData)
         XCTAssertEqual("User1", adminData.message[0].username)
     }
+    
+    func test_get_single_user() throws {
+        guard let path =  Bundle(for: type(of: self)).path(forResource: "singleUser", ofType: "json") else { fatalError("Json Not Found") }
+        guard let json = try? String(contentsOfFile: path, encoding: .utf8) else { return }
+        let jsonData =  json.data(using: .utf8)!
+    }
 }
